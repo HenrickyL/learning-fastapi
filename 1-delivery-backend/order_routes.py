c485ceca0af8eef8ee0b5b0cd6da501b5610a5af
HenrickyL <Henrickyl1@gmail.com>
2025-08-20 23:38:54 -0300

    feat: add get_order route example

--- 1-delivery-backend/order_routes.py
+++ 1-delivery-backend/order_routes.py
@@ -1,3 +1,12 @@
 from fastapi import APIRouter
 
 order_router = APIRouter(prefix="/orders", tags=["Orders"])
+
+# routes
+@order_router.get("/")
+async def get_orders():
+    '''
+    This endpoint returns a list of orders
+    1. GET /orders
+    '''
+    return {"message": "List of orders"}
